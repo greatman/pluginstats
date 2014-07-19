c2215282c01940d6a8c9982827565220700cae19
William Lebel <lebelwilliam@gmail.com>
2014-07-19 17:38:46 -0400

    Index page mostly done. Added a route for /info. Fixed links on the left.

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,14 @@ var router = express.Router();
 router.get('/', function(req, res) {
     plugin.count({}, function(err, c) {
         console.log(c);
-        res.render('index', { title: 'Statik', numberOfPlugins: c });
+        plugin.distinct('entry.guid').count({}, function(err, serverCount) {
+            res.render('index', { title: 'Statik', numberOfPlugins: c, numberOfServers: serverCount });
+        });
     });
+});
+
+router.get('/info', function(req, res) {
+   res.render('info', { title: 'Statik - More Information'});
 });
 
 module.exports = router;
